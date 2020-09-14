faa94b7d0b5c47bc29316f5e37fcbbaf9be3c5a3
Te-Wei Tsai <ttsai@lsst.org>
2020-09-14 16:45:49 -0700

    Add the mapSensorAndFieldIdx() to SourceProcessor.py.

--- examples/mapSensorAndFieldIdxLsstFam.py
+++ examples/mapSensorAndFieldIdxLsstFam.py
@@ -19,27 +19,18 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import os
 import numpy as np
 
+from lsst.ts.wep.Utility import getModulePath
 from lsst.ts.wep.SourceProcessor import SourceProcessor
 
 
 if __name__ == "__main__":
 
-    # Set the 35 field points
-    nArm = 6
-    armLen = [0.379, 0.841, 1.237, 1.535, 1.708]
-    fieldWFSx = [1.176, -1.176, -1.176, 1.176]
-    fieldWFSy = [1.176, 1.176, -1.176, -1.176]
-    pointAngle = np.arange(nArm) * (2 * np.pi) / nArm
-    fieldX = np.concatenate(
-        [np.zeros(1), np.kron(armLen, np.cos(pointAngle)), fieldWFSx]
-    )
-    fieldY = np.concatenate(
-        [np.zeros(1), np.kron(armLen, np.sin(pointAngle)), fieldWFSy]
-    )
-
-    fieldXY = np.array([fieldX, fieldY]).T
+    # Set the 189 field points
+    fieldPosFilePath = os.path.join(getModulePath(), "examples", "fieldPosLsst.txt")
+    fieldXY = np.loadtxt(fieldPosFilePath)
 
     # Do the mapping
     sourPro = SourceProcessor()

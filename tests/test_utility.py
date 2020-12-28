b60947a070ac032a580c6a7784b5993b4fafae99
Te-Wei Tsai <twtsaia@gmail.com>
2020-12-28 10:13:10 -0700

    Update the test cases.

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -23,8 +23,6 @@ import os
 import unittest
 
 from lsst.ts.wep.Utility import (
-    abbrevDectectorName,
-    expandDetectorName,
     mapFilterRefToG,
     FilterType,
     getModulePath,
@@ -43,26 +41,6 @@ from lsst.ts.wep.Utility import (
 
 class TestUtility(unittest.TestCase):
     """Test the Utility functions."""
-
-    def testAbbrevDectectorName(self):
-
-        sciSensorName = "R:2,2 S:1,1"
-        self.assertEqual(abbrevDectectorName(sciSensorName), "R22_S11")
-
-        wfsSensorName = "R:4,0 S:0,2,B"
-        self.assertEqual(abbrevDectectorName(wfsSensorName), "R40_S02_C1")
-
-        self.assertRaises(ValueError, abbrevDectectorName, "R:4,0 S:0,2,C")
-
-    def testExpandDetectorName(self):
-
-        sciSensorName = "R22_S11"
-        self.assertEqual(expandDetectorName(sciSensorName), "R:2,2 S:1,1")
-
-        wfsSensorName = "R40_S02_C1"
-        self.assertEqual(expandDetectorName(wfsSensorName), "R:4,0 S:0,2,B")
-
-        self.assertRaises(ValueError, expandDetectorName, "R40_S02_C2")
 
     def testMapFilterRefToG(self):
 

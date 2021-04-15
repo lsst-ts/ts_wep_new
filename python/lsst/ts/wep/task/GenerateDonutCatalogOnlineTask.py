7c5fb53fd1c99f9ad081b3a082b73cd2c728bf14
Bryce Kalmbach <jbkalmbach@gmail.com>
2021-04-14 17:45:56 -0700

    Moved Task to new task module.

--- python/lsst/ts/wep/task/GenerateDonutCatalogOnlineTask.py
+++ python/lsst/ts/wep/task/GenerateDonutCatalogOnlineTask.py
@@ -1,9 +1,10 @@
+# This file is part of ts_wep.
 #
-# LSST Data Management System
-# Copyright 2008-2017 AURA/LSST.
-#
-# This product includes software developed by the
-# LSST Project (http://www.lsst.org/).
+# Developed for the LSST Telescope and Site Systems.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,23 +16,20 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-# You should have received a copy of the LSST License Statement and
-# the GNU General Public License along with this program.  If not,
-# see <https://www.lsstcorp.org/LegalNotices/>.
-#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import typing
 import os
 import numpy as np
 import pandas as pd
-import lsst.geom
 import lsst.afw.table as afwTable
 import lsst.pex.config as pexConfig
 import lsst.pipe.base as pipeBase
 import lsst.pipe.base.connectionTypes as connectionTypes
 import lsst.obs.lsst as obs_lsst
 from lsst.meas.algorithms import ReferenceObjectLoader, LoadReferenceObjectsConfig
-from lsst.obs.base import createInitialSkyWcsFromBoresight
+from lsst.ts.wep.bsc.WcsSol import WcsSol
 
 
 class GenerateDonutCatalogOnlineTaskConnections(
@@ -81,10 +79,17 @@ class GenerateDonutCatalogOnlineTask(pipeBase.PipelineTask):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+        # The filter in the reference catalog we want to use to find sources.
         self.filterName = self.config.filterName
+
+        # Pointing information to construct the WCS. All values in degrees.
         self.boresightRa = self.config.boresightRa
         self.boresightDec = self.config.boresightDec
         self.boresightRotAng = self.config.boresightRotAng
+
+        # Need camera name to get the detectors to use to find X,Y pixel
+        # values for the sources
         self.cameraName = self.config.cameraName
 
         # TODO: Temporary until DM-24162 is closed at which point we
@@ -93,10 +98,25 @@ class GenerateDonutCatalogOnlineTask(pipeBase.PipelineTask):
 
     def filterResults(self, resultsDataFrame):
         """
-        Here is where we will set up specifications for the sources
-        we want to use (i.e., filter on magnitude, blended, etc.).
-        For now it just returns all sources.
+        Run filtering on full set of sources on detector and return
+        the dataframe with only sources that are acceptable for
+        wavefront estimation.
+
+        Parameters
+        ----------
+        resultsDataFrame: pandas DataFrame
+            Full list of sources from reference catalog that appear
+            on the detector.
+
+        Returns
+        -------
+        pandas DataFrame
+            Subset of resultsDataFrame sources that pass required filtering.
         """
+
+        # TODO: Here is where we will set up specifications for the sources
+        # we want to use (i.e., filter on magnitude, blended, etc.).
+        # For now it just returns all sources.
 
         return resultsDataFrame
 
@@ -125,26 +145,24 @@ class GenerateDonutCatalogOnlineTask(pipeBase.PipelineTask):
 
         # Get camera. Only 'lsstCam' for now.
         if self.cameraName == "lsstCam":
-            lsst_cam = obs_lsst.LsstCam.getCamera()
+            camera = obs_lsst.LsstCam.getCamera()
         else:
             raise ValueError(f"{self.cameraName} is not a valid camera name.")
 
-        for detector in lsst_cam:
-            # Create WCS from boresight information
-            # NOTE: 90.0 - boresightRotAng is a phosim artifact that is being fixed
-            # TODO: Fix this when DM-29702 is merged
-            detWcs = createInitialSkyWcsFromBoresight(
-                lsst.geom.SpherePoint(
-                    self.boresightRa, self.boresightDec, lsst.geom.degrees
-                ),
-                (90.0 - self.boresightRotAng) * lsst.geom.degrees,
-                detector,
+        # Create WCS holder
+        detWcs = WcsSol(camera=camera)
+
+        for detector in camera:
+            # Set WCS from boresight information
+            detWcs.setObsMetaData(
+                self.boresightRa, self.boresightDec, self.boresightRotAng,
+                centerCcd=detector.getName()
             )
 
             try:
                 # Match detector layout to reference catalog
                 donutCatalog = refObjLoader.loadPixelBox(
-                    detector.getBBox(), detWcs, filterName=self.filterName
+                    detector.getBBox(), detWcs.skyWcs, filterName=self.filterName
                 ).refCat
 
                 # Add matched information to list
@@ -164,6 +182,9 @@ class GenerateDonutCatalogOnlineTask(pipeBase.PipelineTask):
         fieldObjects["centroid_y"] = np.hstack(centroidY).squeeze()
         fieldObjects["detector"] = np.hstack(det_names).squeeze()
 
+        # Return pandas DataFrame with sources in pointing
+        # with ra, dec, filter flux, pixel XY information and detector name
+        # for each source
         finalSources = self.filterResults(fieldObjects)
 
         return pipeBase.Struct(donutCatalog=finalSources)

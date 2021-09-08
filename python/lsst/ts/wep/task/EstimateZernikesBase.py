Fix rotation in EstimateZernikesBase. Remove SourceProcessor dependency in tasks.

python/lsst/ts/wep/task/EstimateZernikesBase.py
@@ -29,7 +29,6 @@ from lsst.daf.base import PropertyList
 from lsst.pipe.base import connectionTypes
 
 from lsst.ts.wep.WfEstimator import WfEstimator
-from lsst.ts.wep.SourceProcessor import SourceProcessor
 from lsst.ts.wep.Utility import getConfigDir, DonutTemplateType, DefocalType
 from lsst.ts.wep.cwfs.DonutTemplateFactory import DonutTemplateFactory
 from scipy.signal import correlate
@@ -380,39 +379,30 @@ class EstimateZernikesBaseTask(pipeBase.PipelineTask):
         algoDir = os.path.join(configDir, "cwfs", "algo")
         wfEsti = WfEstimator(instDir, algoDir)
         wfEsti.config(sizeInPix=self.donutStampSize)
-        sourProcExtra = SourceProcessor()
-        sourProcIntra = SourceProcessor()
 
         for donutExtra, donutIntra in zip(donutStampsExtra, donutStampsIntra):
 
-            sensorNameExtra = donutExtra.detector_name
-            sensorNameIntra = donutIntra.detector_name
-            sourProcExtra.config(sensorName=sensorNameExtra)
-            sourProcIntra.config(sensorName=sensorNameIntra)
+            fieldXYExtra = donutExtra.calcFieldXY()
+            fieldXYIntra = donutIntra.calcFieldXY()
 
-            centroidXYExtra = donutExtra.centroid_position
-            centroidXYIntra = donutIntra.centroid_position
+            camera = donutExtra.getCamera()
+            detectorExtra = camera.get(donutExtra.detector_name)
+            detectorIntra = camera.get(donutIntra.detector_name)
+
+            eulerZExtra = detectorExtra.getOrientation().getYaw().asDegrees()
+            eulerZIntra = detectorIntra.getOrientation().getYaw().asDegrees()
 
             # NOTE: TS_WEP expects these images to be transposed
             # TODO: Look into this
-            fieldXYExtra = sourProcExtra.camXYtoFieldXY(
-                centroidXYExtra.getY(), centroidXYExtra.getX()
-            )
-            fieldXYIntra = sourProcIntra.camXYtoFieldXY(
-                centroidXYIntra.getY(), centroidXYIntra.getX()
-            )
-            eulerZExtra = sourProcExtra.getEulerZinDeg(sensorNameExtra)
-            eulerZIntra = sourProcIntra.getEulerZinDeg(sensorNameIntra)
-
             wfEsti.setImg(
                 fieldXYExtra,
                 DefocalType.Extra,
-                image=rotate(donutExtra.stamp_im.getImage().getArray().T, eulerZExtra),
+                image=rotate(donutExtra.stamp_im.getImage().getArray(), eulerZExtra).T,
             )
             wfEsti.setImg(
                 fieldXYIntra,
                 DefocalType.Intra,
-                image=rotate(donutIntra.stamp_im.getImage().getArray().T, eulerZIntra),
+                image=rotate(donutIntra.stamp_im.getImage().getArray(), eulerZIntra).T,
             )
             wfEsti.reset()
             zer4UpNm = wfEsti.calWfsErr()
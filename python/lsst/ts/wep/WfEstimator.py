d2f3dc0879b32c0cf6871ab4de7578ada4c7cb23
Chris Suberlak <suberlak@uw.edu>
2022-12-15 16:38:05 -0800

    throw valueError with offAxis for auxTel

--- python/lsst/ts/wep/WfEstimator.py
+++ python/lsst/ts/wep/WfEstimator.py
@@ -171,12 +171,16 @@ class WfEstimator(object):
         """
 
         if solver not in ("exp", "fft"):
-            raise ValueError("Poisson solver can not be '%s'." % solver)
+            raise ValueError("Poisson solver cannot be '%s'." % solver)
 
         if opticalModel not in ("paraxial", "onAxis", "offAxis"):
-            raise ValueError("Optical model can not be '%s'." % opticalModel)
+            raise ValueError("Optical model cannot be '%s'." % opticalModel)
         else:
             self.opticalModel = opticalModel
+
+        if camType == CamType.AuxTel:
+            if opticalModel not in ("paraxial", "onAxis"):
+                raise ValueError("Optical model cannot be '%s' for AuxTel." % opticalModel)
 
         # Update the instrument name
         self.sizeInPix = int(sizeInPix)

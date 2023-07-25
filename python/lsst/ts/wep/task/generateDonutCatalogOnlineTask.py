403f305e5d6414172131fdc4c3e8defa3d6babcf
Chris Suberlak <suberlak@uw.edu>
2023-07-25 08:23:46 -0700

    black fixes

--- python/lsst/ts/wep/task/generateDonutCatalogOnlineTask.py
+++ python/lsst/ts/wep/task/generateDonutCatalogOnlineTask.py
@@ -25,13 +25,13 @@ import warnings
 
 import lsst.pex.config as pexConfig
 import lsst.pipe.base as pipeBase
-from lsst.utils.timer import timeMethod
-from lsst.meas.algorithms import (
-    ReferenceObjectLoader,
-)
-
+from lsst.meas.algorithms import ReferenceObjectLoader
 from lsst.ts.wep.task.donutSourceSelectorTask import DonutSourceSelectorTask
-from lsst.ts.wep.task.generateDonutCatalogUtils import donutCatalogToDataFrame, runSelection
+from lsst.ts.wep.task.generateDonutCatalogUtils import (
+    donutCatalogToDataFrame,
+    runSelection,
+)
+from lsst.utils.timer import timeMethod
 
 
 class GenerateDonutCatalogOnlineTaskConfig(pexConfig.Config):
@@ -108,7 +108,9 @@ class GenerateDonutCatalogOnlineTask(pipeBase.Task):
 
         try:
             self.log.info("Running Donut Selector")
-            donutSelectorTask = self.donutSelector if self.config.doDonutSelection is True else None
+            donutSelectorTask = (
+                self.donutSelector if self.config.doDonutSelection is True else None
+            )
             refSelection, blendCentersX, blendCentersY = runSelection(
                 refObjLoader, detector, detectorWcs, filterName, donutSelectorTask
             )

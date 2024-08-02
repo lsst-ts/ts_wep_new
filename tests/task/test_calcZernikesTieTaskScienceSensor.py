5e0f96b556dbbe0a5334dcae073ebf67c7ef9a79
Chris Suberlak <suberlak@uw.edu>
2024-08-02 04:57:54 +0000

    update tests for calcZernikesTieTaskScienceSensor

--- tests/task/test_calcZernikesTieTaskScienceSensor.py
+++ tests/task/test_calcZernikesTieTaskScienceSensor.py
@@ -23,6 +23,7 @@ import os
 
 import lsst.utils.tests
 import numpy as np
+import pandas as pd
 from lsst.daf import butler as dafButler
 from lsst.ts.wep.task import (
     CalcZernikesTask,
@@ -107,6 +108,23 @@ class TestCalcZernikesTieTaskScienceSensor(lsst.utils.tests.TestCase):
 
         self.assertEqual(type(self.task.combineZernikes), CombineZernikesMeanTask)
 
+        # Test the default config values
+        self.assertEqual(self.task.selectWithEntropy, False)
+        self.assertEqual(self.task.selectWithSignalToNoise, False)
+        self.assertEqual(self.task.useCustomSnLimit, False)
+
+        # Test changing configs
+        self.config.selectWithEntropy = True
+        self.config.selectWithSignalToNoise = True
+        self.config.minSignalToNoise = 500
+        self.config.maxEntropy = 4
+        self.task = CalcZernikesTask(config=self.config, name="Changed Task")
+
+        self.assertEqual(self.task.selectWithEntropy, True)
+        self.assertEqual(self.task.selectWithSignalToNoise, True)
+        self.assertEqual(self.task.minSignalToNoise, 500)
+        self.assertEqual(self.task.maxEntropy, 4)
+
     def testEstimateZernikes(self):
         donutStampsExtra = self.butler.get(
             "donutStampsExtra", dataId=self.dataIdExtra, collections=[self.runName]
@@ -124,6 +142,46 @@ class TestCalcZernikesTieTaskScienceSensor(lsst.utils.tests.TestCase):
 
         self.assertEqual(np.shape(zernCoeff), (len(donutStampsExtra), 19))
 
+    def testCalcZernikes(self):
+        donutStampsExtra = self.butler.get(
+            "donutStampsExtra", dataId=self.dataIdExtra, collections=[self.runName]
+        )
+        donutStampsIntra = self.butler.get(
+            "donutStampsIntra", dataId=self.dataIdExtra, collections=[self.runName]
+        )
+        structNormal = self.task.run(donutStampsIntra, donutStampsExtra)
+
+        # check that 4 elements are created
+        self.assertEqual(len(structNormal), 4)
+
+        # check that donut quality is reported for all donuts
+        self.assertEqual(len(structNormal.donutsExtraQuality), len(donutStampsExtra))
+        self.assertEqual(len(structNormal.donutsIntraQuality), len(donutStampsIntra))
+
+        # check that all desired quantities are included
+        colnames = list(structNormal.donutsIntraQuality.columns)
+        desired_colnames = [
+            "SN",
+            "ENTROPY",
+            "ENTROPY_SELECT",
+            "SN_SELECT",
+            "FINAL_SELECT",
+        ]
+        np.testing.assert_array_equal(np.sort(colnames), np.sort(desired_colnames))
+
+        # test null run
+        structNull = self.task.run([], [])
+
+        for struct in [structNormal, structNull]:
+            # test that in accordance with declared connections,
+            # donut quality tables are pandas dataFrame,
+            # and Zernikes are numpy arrays
+            # both for normal run and for null run
+            self.assertIsInstance(struct.donutsIntraQuality, pd.DataFrame)
+            self.assertIsInstance(struct.donutsExtraQuality, pd.DataFrame)
+            self.assertIsInstance(struct.outputZernikesRaw, np.ndarray)
+            self.assertIsInstance(struct.outputZernikesAvg, np.ndarray)
+
     def testGetCombinedZernikes(self):
         testArr = np.zeros((2, 19))
         testArr[1] += 2.0
@@ -134,3 +192,59 @@ class TestCalcZernikesTieTaskScienceSensor(lsst.utils.tests.TestCase):
         np.testing.assert_array_equal(
             combinedZernikesStruct.flags, np.zeros(len(testArr))
         )
+
+    def testSelectDonuts(self):
+        donutStampsIntra = self.butler.get(
+            "donutStampsIntra", dataId=self.dataIdExtra, collections=[self.runName]
+        )
+
+        # test default: no donuts are excluded
+        donutStampsSelect, donutsQuality = self.task.selectDonuts(donutStampsIntra)
+
+        # by default,  config.selectWithEntropy is False,
+        # so we select all donuts
+        self.assertEqual(np.sum(donutsQuality["ENTROPY_SELECT"]), 3)
+
+        # by default, config.selectWithSignalToNoise is False,
+        # so we select all donuts
+        self.assertEqual(np.sum(donutsQuality["SN_SELECT"]), 3)
+
+        # switch on selectWithEntropy,
+        # set config.maxEntropy so that one donut is selected
+        self.config.selectWithEntropy = True
+        entropyThreshold = 2.8
+        self.config.maxEntropy = entropyThreshold
+
+        task = CalcZernikesTask(config=self.config, name="Entropy Task")
+
+        donutStampsSelect, donutsQuality = task.selectDonuts(donutStampsIntra)
+        self.assertEqual(np.sum(donutsQuality["ENTROPY_SELECT"]), 1)
+
+        # also test that the entropy of the selected donut
+        # is indeed below threshold
+        self.assertLess(
+            donutsQuality["ENTROPY"][donutsQuality["ENTROPY_SELECT"]].values,
+            entropyThreshold,
+        )
+
+        # switch on selectWithSignalToNoise
+        self.config.selectWithSignalToNoise = True
+        task = CalcZernikesTask(config=self.config, name="SN Task")
+
+        # by default we use the yaml config values so that
+        # all donuts here would get selected
+        donutStampsSelect, donutsQuality = task.selectDonuts(donutStampsIntra)
+        self.assertEqual(np.sum(donutsQuality["SN_SELECT"]), 3)
+
+        # test that if we use the custom threshold,
+        # some donuts won't get selected
+        self.config.useCustomSnLimit = True
+        minSignalToNoise = 1000
+        self.config.minSignalToNoise = minSignalToNoise
+        task = CalcZernikesTask(config=self.config, name="Base Task")
+        donutStampsSelect, donutsQuality = task.selectDonuts(donutStampsIntra)
+        self.assertEqual(np.sum(donutsQuality["SN_SELECT"]), 2)
+
+        # test that the SN of selected donuts is indeed above the threshold
+        for v in donutsQuality["SN"][donutsQuality["SN_SELECT"]].values:
+            self.assertLess(minSignalToNoise, v)
